e2e8d6d38f43b886e51f6be2b8777070f2498db4
Oleg Evseev <evs.o@icloud.com>
2017-01-03 21:46:40 +0300

    Epic comeback

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 ;
 (function () {
   captions = [
-    "пригодится, если доживем",
+    "<s>пригодится, если доживем</s> уже дожили",
     "избавленный от недостатков в виде замечательного почерка Александра Ивановича",
     "высоко оцененный критиками мировой бестселлер",
     "с р∀зр∃ш∃ния н∃б∃с",
-    "ради этого конспекта мне пришлось перечитать TeXbook Кнута",
+    // "ради этого конспекта мне пришлось перечитать TeXbook Кнута",
     "видел бы его сам Александр Иванович",
-    "жизнь вымрет быстрее, чем появится следующее обновление конспекта",
-    "the slowest updating summary ever",
-    "на коллоквиуме всем крышка",
-    "colloquium is coming"
+    // "жизнь вымрет быстрее, чем появится следующее обновление конспекта",
+    // "the slowest updating summary ever",
+    "на <s>коллоквиуме</s> экзамене всем крышка (на самом деле нет)",
+    "<s>colloquium</s> exam is coming",
+    "полная опечаток писанина",
+    "сейчас бы в 2k17 сдавать экзамен по АТЧ"
   ];
 
   function loadJSON(url, callback) {
@@ -36,17 +38,19 @@
   }
 
   function loadData(data) {
-    //var date = new Date(data.lastUpdate);
-    //var dateString = "(обновлено " + date.toLocaleString() + ")";
+    var date = new Date(data.lastUpdate);
+    var dateString = "(обновлено " + date.toLocaleString() + ")";
 
-    //document.getElementById('content-date').innerHTML = dateString;
+    document.getElementById('content-date').innerHTML = dateString;
     document.getElementById('content-link').setAttribute('href', data.contentLink);
     document.getElementById('content-summary').innerHTML = data.summary;
+
+    document.getElementById('github-link').setAttribute('href', data.githubLink);
   }
 
   function initialize() {
     setRandomCaption();
-    loadJSON('data/summary.json', loadData);
+    loadJSON('data/summary.json?application/xhtml+xml', loadData);
   }
 
   window.onload = initialize;
